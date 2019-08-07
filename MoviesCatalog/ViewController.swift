fcfaf0f1a39a8157fc61292cc28f5e6fd125accf
Hind <hind@hinds-macbook-pro.local>
2019-08-07 09:33:08 +0200

    Initial Commit

---
+++ MoviesCatalog/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoviesCatalog
+//
+//  Created by Hind on 07/08/2019.
+//  Copyright © 2019 Hind. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
